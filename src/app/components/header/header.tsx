da160e518dba7a9a34256c4c25360138298c483f
lenitaCosta <lenytamarttins@gmail.com>
2024-06-14 23:57:41 -0300

    feat: add initial html structure and header

---
+++ src/app/components/header/header.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import "./header.scss";
+export function Header() {
+  return (
+    <div className="header">
+      <div>
+        <h1>Hi, i'm Lenita!👋</h1>
+        <h2>Software developer</h2>
+      </div>
+      <Image
+        src="/me.jpg"
+        alt="Vercel Logo"
+        width={225}
+        height={270}
+        priority
+      />
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,30 @@
-import Image from "next/image";
-
+import { Header } from "./components/header/header";
+import "./styles/home.scss";
 export default function Home() {
   return (
-    <main>
-      <Image
-        src="/vercel.svg"
-        alt="Vercel Logo"
-        width={100}
-        height={24}
-        priority
-      />
+    <main className="container">
+      <Header />
+      <div className="experience">
+        <h3>Experience</h3>
+        <p>texto</p>
+        <div className="experience-time"></div>
+        <div className="infos">
+          <h3>Languages</h3>
+          <div className="languages-info">
+            <span> 🇺🇸EN - Fluent</span>
+            <span> 🇧🇷PT-BR- Native Speaker</span>
+          </div>
+          <h3>Education</h3>
+          <div className="education-info">
+            <span>🎓</span>
+            <span>Systems analysis and development - Faculdade Anhanguera</span>
+          </div>
+        </div>
+        <div className="buttons">
+          <div className="social"></div>
+          <button> contact me </button>
+        </div>
+      </div>
     </main>
   );
 }
