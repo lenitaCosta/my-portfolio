4a0bad29a3d692ddf7bd37bacd49db5a212abc13
lenitaCosta <lenytamarttins@gmail.com>
2024-06-16 01:17:41 -0300

    feat: measures

--- src/app/components/experience/Experience.tsx
+++ src/app/components/experience/Experience.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { SectionTitle } from "../sectionTitle/section-title";
 import "./experience.scss";
 
@@ -5,8 +6,57 @@ export function Experience() {
   return (
     <div className="experience">
       <SectionTitle text="Experience" />
-      <p>1 year working as a Software Developer, in company such as Fenasbac</p>
-      <div className="experience-time"></div>
+      <p>1 year working as a Software Developer at Fenasbac</p>
+
+      <div className="experience-time">
+        <div className="experience-language">
+          <Image src="/js.jpg" alt="Js Logo" width={40} height={40} priority />
+          <div className="experience-unit">
+            <div className="experience-measure measure-1">
+              <span>1 year</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="experience-language">
+          <Image src="/ts.jpg" alt="Ts Logo" width={40} height={40} priority />
+          <div className="experience-unit">
+            <div className="experience-measure measure-2">
+              <span>1 year</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="experience-language">
+          <Image
+            src="/node.jpg"
+            alt="Node Logo"
+            width={40}
+            height={40}
+            priority
+          />
+          <div className="experience-unit">
+            <div className="experience-measure measure-3">
+              <span>1 year</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="experience-language">
+          <Image
+            src="/nest.jpg"
+            alt="Nest Logo"
+            width={40}
+            height={40}
+            priority
+          />
+          <div className="experience-unit">
+            <div className="experience-measure measure-4">
+              <span>1 year</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import "./header.scss";
+
 export function Header() {
   return (
     <div className="header">
       <div>
-        <h1>Hi, i'm Lenita!👋</h1>
+        <h1>Hi, I&apos;m Lenita!👋</h1> {}
         <h2>Software developer</h2>
       </div>
       <Image
